2baac2368260ca92f8470cc30fe8bb659d0736fd
haekalpks <hkalipaksi@outlook.com>
2022-10-23 12:13:06 +0700

    added responsive

--- src/components/elements/Footer/Collaps.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import Link from 'next/link'
-import { Transition } from '@headlessui/react'
-
-export const Collaps: React.FC = ({}) => <div></div>
--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Collaps } from './Collaps'
 import { NavbarBody } from './NavbarBody'
 import { NavbarStateProps } from './interface'
