b1dace92dd4d1ca6152a573cf2c51724807d4ff4
cnotgate <49557224+cnotgate@users.noreply.github.com>
2022-10-26 00:30:45 +0700

    Changes to be committed: 	modified:   next.config.js 	new file:   public/assets/images/Mask-group.png 	modified:   src/components/elements/Footer/index.tsx 	modified:   src/components/modules/WebteamModule/constant.ts 	modified:   src/components/modules/WebteamModule/index.tsx 	modified:   yarn.lock
    Changes not staged for commit:
    	modified:   .eslintrc.json
    	modified:   .prettierrc
    	modified:   README.md
    	modified:   next.config.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   postcss.config.js
    	modified:   src/components/contexts/index.ts
    	modified:   src/components/elements/Footer/constant.ts
    	modified:   src/components/elements/Footer/index.tsx
    	modified:   src/components/elements/Footer/interface.ts
    	modified:   src/components/elements/Imageoptimized/constant.ts
    	modified:   src/components/elements/Imageoptimized/index.tsx
    	modified:   src/components/elements/Imageoptimized/interface.ts
    	modified:   src/components/elements/Navbar/Collaps.tsx
    	modified:   src/components/elements/Navbar/NavbarBody.tsx
    	modified:   src/components/elements/Navbar/constant.ts
    	modified:   src/components/elements/Navbar/index.tsx
    	modified:   src/components/elements/Navbar/interface.ts
    	modified:   src/components/elements/index.ts
    	modified:   src/components/hooks/index.ts
    	modified:   src/components/icons/Instagram.tsx
    	modified:   src/components/icons/Menu.tsx
    	modified:   src/components/icons/index.ts
    	modified:   src/components/icons/interface.ts
    	modified:   src/components/index.ts
    	modified:   src/components/modules/HomeModule/HeroSection.tsx
    	modified:   src/components/modules/HomeModule/IntroAngkatanSection.tsx
    	modified:   src/components/modules/HomeModule/constant.ts
    	modified:   src/components/modules/HomeModule/index.tsx
    	modified:   src/components/modules/HomeModule/interface.ts
    	modified:   src/components/modules/PlayhardModule/constant.ts
    	modified:   src/components/modules/PlayhardModule/index.tsx
    	modified:   src/components/modules/PlayhardModule/interface.ts
    	modified:   src/components/modules/ThefigureModule/constant.ts
    	modified:   src/components/modules/ThefigureModule/index.tsx
    	modified:   src/components/modules/ThefigureModule/interface.ts
    	modified:   src/components/modules/WebteamModule/constant.ts
    	modified:   src/components/modules/WebteamModule/index.tsx
    	modified:   src/components/modules/WebteamModule/interface.ts
    	modified:   src/components/modules/WorkhardModule/constant.ts
    	modified:   src/components/modules/WorkhardModule/index.tsx
    	modified:   src/components/modules/WorkhardModule/interface.ts
    	modified:   src/components/modules/index.ts
    	modified:   src/components/ssr/index.ts
    	modified:   src/components/utils/index.ts
    	modified:   src/pages/_app.tsx
    	modified:   src/pages/api/hello.ts
    	modified:   src/pages/index.tsx
    	modified:   src/pages/playhard/index.tsx
    	modified:   src/pages/thefigure/index.tsx
    	modified:   src/pages/webteam/index.tsx
    	modified:   src/pages/workhard/index.tsx
    	modified:   src/styles/globals.css
    	modified:   tailwind.config.js
    	modified:   tsconfig.json

--- src/components/modules/WebteamModule/index.tsx
+++ src/components/modules/WebteamModule/index.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Imageoptimized } from '@elements';
+import { dataWebDev } from './constant';
 
 export const WebteamModule: React.FC = () => {
   // TODO: Write module's logic
-
-  return <></>
+  return (
+    <div className="container my-12 align-middle mx-auto py-10 px-4">
+      <header>
+        <h1 className='text-[40px] font-interbold py-5 pb-8 text-center'>
+          Meet Our Web Dev Team
+        </h1>
+      </header>
+      <div className='lg:px-12 md:px-12 grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4'>
+        {dataWebDev.map((key, index) => (
+          <div className="my-1 px-1">
+            <article className="overflow-hidden rounded-2xl shadow-xl bg-[#0E1F58]">
+              <div className='p-4'>
+                {/* <Imageoptimized
+                  imageUrl='https://drive.google.com/uc?export=view&id=1b8XvH3dDS-nHD64Dc_pNHcDmtr7KFDw4'
+                  className='rounded-full'
+                  alt="foto"
+                  fit="cover"
+                /> */}
+                <img alt="Placeholder" className="block h-auto w-full rounded-lg aspect-square object-cover" src={"https://drive.google.com/uc?export=view&id="+key['id foto']} />
+                {/* <img alt="Placeholder" className="block h-auto w-full rounded-lg aspect-square object-cover" src='https://drive.google.com/uc?export=view&id=1b8XvH3dDS-nHD64Dc_pNHcDmtr7KFDw4' /> */}
+              </div>
+              <header className=" pl-4 pb-7 md:px-4">
+                <h1 className="lg:text-base md:text-sm sm:text-xs text-xs text-white">
+                  <p className="relative font-interbold truncate" data-tooltip-target="tooltip-light">
+                    {key['Nama Lengkap']}
+                  </p>
+                  
+                </h1>
+                <p className='lg:text-md md:text-sm text-white font-interlight'>
+                  AB12  
+                </p>
+              </header>
+            </article>
+        </div>
+        ))}
+        </div>
+      </div>
+    );
 }
