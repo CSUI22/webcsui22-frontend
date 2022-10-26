898c8186e682542d9949cd47da6a10703aa05055
cnotgate <49557224+cnotgate@users.noreply.github.com>
2022-10-26 21:18:33 +0700

    Added several new features
    Added Cloudinary integration
    Updated webteam JSON data in constant.ts in WebTeamModule
    Added webdev role
    Removed PMB code
    Added hover animation

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['drive.google.com'],
+    domains: ['drive.google.com', 'res.cloudinary.com'],
   },
 }
 
--- src/components/modules/WebteamModule/index.tsx
+++ src/components/modules/WebteamModule/index.tsx
@@ -1,45 +1,47 @@
 import React from 'react'
-import { Imageoptimized } from '@elements';
-import { dataWebDev } from './constant';
+// import { Imageoptimized } from '@elements'
+import { dataWebDev } from './constant'
+import {AdvancedImage, lazyload} from '@cloudinary/react';
+import {Cloudinary} from "@cloudinary/url-gen";
+import {focusOn} from "@cloudinary/url-gen/qualifiers/gravity";
+import {FocusOn} from "@cloudinary/url-gen/qualifiers/focusOn";
+import {thumbnail} from "@cloudinary/url-gen/actions/resize";
+import {byRadius} from "@cloudinary/url-gen/actions/roundCorners";
+
 
 export const WebteamModule: React.FC = () => {
-  // TODO: Write module's logic
+  const myCld = new Cloudinary({cloud: {cloudName: 'djj4bzojc'}});
   return (
     <div className="container my-12 align-middle mx-auto py-10 px-4">
       <header>
-        <h1 className='text-[40px] font-interbold py-5 pb-8 text-center'>
+        <h1 className="text-[40px] font-interbold py-5 pb-8 text-center">
           Meet Our Web Dev Team
         </h1>
       </header>
-      <div className='lg:px-12 md:px-12 grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4'>
+      <div className="lg:px-12 md:px-12 grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
         {dataWebDev.map((key, index) => (
-          <div className="my-1 px-1">
-            <article className="overflow-hidden rounded-2xl shadow-xl bg-[#0E1F58]">
-              <div className='p-4'>
-                {/* <Imageoptimized
-                  imageUrl='https://drive.google.com/uc?export=view&id=1b8XvH3dDS-nHD64Dc_pNHcDmtr7KFDw4'
-                  className='rounded-full'
-                  alt="foto"
-                  fit="cover"
-                /> */}
-                <img alt="Placeholder" className="block h-auto w-full rounded-lg aspect-square object-cover" src={"https://drive.google.com/uc?export=view&id="+key['id foto']} />
-                {/* <img alt="Placeholder" className="block h-auto w-full rounded-lg aspect-square object-cover" src='https://drive.google.com/uc?export=view&id=1b8XvH3dDS-nHD64Dc_pNHcDmtr7KFDw4' /> */}
+          <div key={key['id foto']} className="my-1 px-1">
+            <article className="overflow-hidden rounded-2xl shadow-xl bg-[#0E1F58] transition ease-in-out delay-150hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300">
+              <div className="p-4">
+                <AdvancedImage cldImg={myCld.image(key['id foto']).resize(thumbnail().width(700).height(700).gravity(focusOn(FocusOn.face()))).roundCorners(byRadius(40)).format('png')} plugins={[lazyload()]}/>
               </div>
               <header className=" pl-4 pb-7 md:px-4">
                 <h1 className="lg:text-base md:text-sm sm:text-xs text-xs text-white">
-                  <p className="relative font-interbold truncate" data-tooltip-target="tooltip-light">
+                  <p
+                    className="relative font-interbold truncate"
+                    data-tooltip-target="tooltip-light"
+                  >
                     {key['Nama Lengkap']}
                   </p>
-                  
                 </h1>
-                <p className='lg:text-md md:text-sm text-white font-interlight'>
-                  AB12  
+                <p className="lg:text-sm md:text-xs sm:text-xs text-xs  text-white font-interlight">
+                  {key['role']}
                 </p>
               </header>
             </article>
-        </div>
+          </div>
         ))}
-        </div>
       </div>
-    );
+    </div>
+  )
 }
