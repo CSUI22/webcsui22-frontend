ed3e156a5dff83152d31b825a6631a3c9335503b
cnotgate <49557224+cnotgate@users.noreply.github.com>
2022-10-31 00:20:26 +0700

    Added several features and change coloring

--- src/components/modules/ThefigureModule/menu.tsx
+++ src/components/modules/ThefigureModule/menu.tsx
@@ -31,7 +31,7 @@ export default function MenuComponent() {
                 {({ active }) => (
                   <button
                     className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                      active ? 'bg-[] text-white' : 'text-gray-900'
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
                     {active ? (
