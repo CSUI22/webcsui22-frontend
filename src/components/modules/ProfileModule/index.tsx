59b988f5bfd9f380aa0d56b5ed1edd91039db579
cnotgate <49557224+cnotgate@users.noreply.github.com>
2022-11-03 11:48:50 +0700

    Added dynamic routing in ProfileModule

--- src/components/modules/ProfileModule/index.tsx
+++ src/components/modules/ProfileModule/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const ProfileModule = ({id}) => {
-  // TODO: Write module's logic
-
+export const ProfileModule = ({ id }: any) => {
   return (
-    <div className='pt-[10em]'>
-      {id["Nama Lengkap"]}
+    <div className="pt-[10em]">
+      {id['Nama Lengkap']}
       <br />
-      {id["Tanggal Lahir"]}
+      {id['Tanggal Lahir']}
       <br />
-      {id["Deskripsi Singkat"]}
+      {id['Deskripsi Singkat']}
     </div>
   )
+}
+
+ProfileModule.propTypes = {
+  id: PropTypes.object,
 }
--- src/pages/thefigure/[idfoto].tsx
+++ src/pages/thefigure/[idfoto].tsx
@@ -5,14 +5,14 @@ import { dataAngkatan } from 'src/components/modules/ThefigureModule/constant'
 const Post = () => {
   const router = useRouter()
   const { idfoto } = router.query
-	let data
-	dataAngkatan.some((key) => {
-		if(key['id foto'] === idfoto) {
-			data = key;
-			return true;
-		}
-	})
-  return <ProfileModule id={data}/>
+  let data
+  dataAngkatan.some((key) => {
+    if (key['id foto'] === idfoto) {
+      data = key
+      return true
+    }
+  })
+  return <ProfileModule id={data} />
 }
 
 export default Post
