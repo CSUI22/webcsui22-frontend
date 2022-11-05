cba1441e535e049ba367b9c2bb21cc99b955a68d
DimasHN_04 <112958430+dimashn04@users.noreply.github.com>
2022-11-05 22:16:27 +0700

    Update index.tsx

--- src/components/modules/PlayhardModule/index.tsx
+++ src/components/modules/PlayhardModule/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Event } from './event'
 
 export const PlayhardModule: React.FC = () => {
   // TODO: Write module's logic
 
-  return <></>
+  return (
+    <>
+        <h1 className="text-[80px] font-interbold  text-center pt-40">
+          Play Hard
+        </h1>
+        <p className='text-[24px] text-center'>
+          Until we meet again!
+        </p>
+        <div className="flex flex-col gap-16 pt-10">
+          <Event />
+        </div>
+        <h1 className="text-[72px] font-interbold  text-center pt-40 pb-40">
+          Sabtu Pelangi
+        </h1>
+      </>
+    )
 }
